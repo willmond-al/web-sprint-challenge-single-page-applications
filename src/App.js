e1d8f273b216cad2be994d7cc302c2acd353ae16
willmondal <wbmondal@aol.com>
2020-12-18 08:26:01 -0800

    home and pizza links made

--- src/App.js
+++ src/App.js
@@ -1,11 +1,23 @@
 import React from "react";
+import { Route, Link } from 'react-router-dom'
+import Home from './Home'
+import Pizza from './Pizza'
 
 const App = () => {
   return (
-    <>
+    <div className="App">
       <h1>Lambda Eats</h1>
-      <p>You can remove this code and create your own header</p>
-    </>
+      <Link to='/'>Home</Link>
+      <Link to='/pizza'>Order Now</Link>
+      <Route path='/pizza'>
+        <Pizza/>
+      </Route>
+      <Route path='/'>
+        <Home/>
+      </Route>
+
+    </div>
+    
   );
 };
 export default App;
